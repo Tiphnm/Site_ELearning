8c49843c72639087b32f885763676522b0045b47
Tiphnm <76214478+Tiphnm@users.noreply.github.com>
2021-02-23 12:34:54 +0100

    Ajout de mon fichier database, création de mes 3 tables

---
+++ db.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+                                host="localhost",
+                                database= "My_classes",
+                                user="root",
+                                password="123")
+
+mycursor= database.cursor()
+
+def create_table():
+    mycursor.execute("CREATE TABLE IF NOT EXISTS Javascript (ID INT PRIMARY KEY AUTO_INCREMENT, TITLE VARCHAR(255), LINK VARCHAR(255));")
+    mycursor.execute("CREATE TABLE IF NOT EXISTS Python (ID INT PRIMARY KEY AUTO_INCREMENT, TITLE VARCHAR(255), LINK VARCHAR(255));")
+    mycursor.execute("CREATE TABLE IF NOT EXISTS Azure (ID INT PRIMARY KEY AUTO_INCREMENT, TITLE VARCHAR(255), LINK VARCHAR(255));")
+
+create_table()
